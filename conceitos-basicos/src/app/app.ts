f7089a7c3fa143ffcb4181a40c70c26657f2caed
Guilherme Sa <guicarrer@yahoo.com.br>
2025-08-24 15:01:57 -0300

    Secao 2 - Item 20

--- conceitos-basicos/src/app/app.ts
+++ conceitos-basicos/src/app/app.ts
@@ -1,10 +1,10 @@
 import {Component, signal} from '@angular/core';
-import {Calculadora} from './calculadora/calculadora';
+import {ListaCompras} from './lista-compras/lista-compras';
 
 @Component({
   selector: 'app-root',
   imports: [
-    Calculadora
+    ListaCompras
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
---
+++ conceitos-basicos/src/app/lista-compras/lista-compras.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {ItemLista} from './itemLista';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-lista-compras',
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
+  templateUrl: './lista-compras.html',
+  standalone: true,
+  styleUrl: './lista-compras.scss'
+})
+export class ListaCompras {
+
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem() {
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+    this.lista.push(itemLista);
+    this.item = '';
+
+    console.table(this.lista);
+  }
+
+}
