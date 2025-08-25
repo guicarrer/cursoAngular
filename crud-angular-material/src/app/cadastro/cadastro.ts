cbe703a9a508b9979ccc18a1c84ee5e2aae7bfc3
Guilherme Sa <guicarrer@yahoo.com.br>
2025-08-25 20:33:30 -0300

    Secao 3 - Item 27

--- crud-angular-material/src/app/cadastro/cadastro.ts
+++ crud-angular-material/src/app/cadastro/cadastro.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormField,
+    MatLabel,
+    MatInputModule
+  ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss'
 })
