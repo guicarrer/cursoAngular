f1ee1fd05ec33d8425aa469e4249c2b1d2101e4f
Guilherme Sa <guicarrer@yahoo.com.br>
2025-08-26 20:35:06 -0300

    Secao 3 - Item 34

--- crud-angular-material/src/app/consulta/consulta.ts
+++ crud-angular-material/src/app/consulta/consulta.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    FormsModule,
+  ],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss'
 })
