e2f9944520ce881116a9b3d805e5b87ff5494c7d
Guilherme Sa <guicarrer@yahoo.com.br>
2026-01-14 12:30:30 -0300

    Secao 7 - Arquitetura de Micro-Frontends com Module-Federation - Criando a pagina principal do projeto

--- microfront-ends/produtos/src/app/app.ts
+++ microfront-ends/produtos/src/app/app.ts
@@ -1,11 +1,52 @@
 import {Component, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+interface Produto {
+  nome: string;
+  valor: number;
+  imagem: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('produtos');
+  produtos: Produto[] = [
+    {
+      nome: 'Câmera Fotográfica',
+      valor: 1500,
+      imagem: 'https://picsum.photos/id/1/200/150'
+    },
+    {
+      nome: 'Smartphone',
+      valor: 2500,
+      imagem: 'https://picsum.photos/id/2/200/150'
+    },
+    {
+      nome: 'Notebook',
+      valor: 4500,
+      imagem: 'https://picsum.photos/id/3/200/150'
+    },
+    {
+      nome: 'Tablet',
+      valor: 1200,
+      imagem: 'https://picsum.photos/id/4/200/150'
+    },
+    {
+      nome: 'Smartwatch',
+      valor: 800,
+      imagem: 'https://picsum.photos/id/5/200/150'
+    },
+    {
+      nome: 'Fone de Ouvido',
+      valor: 300,
+      imagem: 'https://picsum.photos/id/6/200/150'
+    }
+  ];
+
+
+
 }
