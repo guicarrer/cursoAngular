adcc6add271cec046632e815f4b0d54d17b7c312
Guilherme Sa <guicarrer@yahoo.com.br>
2026-01-14 18:49:36 -0300

    Secao 7 - Arquitetura de Micro-Frontends com Module-Federation - Implementando o grafico de vendas

--- microfront-ends/grafico/src/app/app.ts
+++ microfront-ends/grafico/src/app/app.ts
@@ -1,4 +1,5 @@
-import {Component, signal} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Chart} from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,44 @@ import {Component, signal} from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('grafico');
+export class App implements OnInit {
+
+  ngOnInit(): void {
+    const dados = [
+      {categoria: 'Eletronicos', valor: 30},
+      {categoria: 'Roupas', valor: 20},
+      {categoria: 'Alimentos', valor: 50},
+      {categoria: 'Livros', valor: 15},
+      {categoria: 'Móveis', valor: 25},
+      {categoria: 'Brinquedos', valor: 10},
+      {categoria: 'Esportes', valor: 18},
+      {categoria: 'Beleza', valor: 22}
+    ];
+
+    const labels = dados.map(v => v.categoria);
+    const valores = dados.map(v => v.valor);
+
+    new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Vendas por Categoria',
+            data: valores,
+            backgroundColor: 'rgba(75, 192, 192, 0.6)',
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+  }
 }
